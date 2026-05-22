11227b04b56b24538b3cbfac987e204d171cb421
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2026-05-22 10:54:25 +0200

    test d'intégration

--- test/test_zone1.py
+++ test/test_zone1.py
@@ -2,29 +2,56 @@
 
 import unittest
 
+import os
 import tracklib as tkl
+from footprint2graph import setupenv
+
 
 class TestZone1(unittest.TestCase):
-    
+    '''
+    Test complet sur un jeu de traces simulées à partir d’un réseau.
+    C'est le code du notebook dans la documentation
+    '''
 
 
     def setUp (self):
-        #RESPATH = r'/home/md_vandamme/4_RESEAU/ZTEMPZ1/'
-        pass
+        RESPATH = r'./result'
+        setupenv(RESPATH)
 
-        
+        #  Import du réseau
+        netpath = os.path.abspath(os.path.join('../data/network2.csv'))
+        fmt = tkl.NetworkFormat({
+               "pos_edge_id": 1,
+               "pos_source": 2,
+               "pos_target": 3,
+               "pos_wkt": 0,
+               "srid": "ENU",
+               "separator": ";",
+               "header": 1})
+
+        self.network = tkl.NetworkReader.readFromFile(netpath, fmt, verbose=False)
+
+        # Génération des traces réalistes synthétiques
+        tkl.stochastics.seed(333)
+        noiser = tkl.NoiseProcess(amps=2.5, kernels=tkl.ExponentialKernel(80))
+
+        # generate simulated trajectories from the network
+        collection = tkl.generateTracksOnNetwork(self.network, N=100, p_round_trip=0.05, p_cplx_trip=0.10, resolution=1, noiser=noiser)
+        # add 3 attributes
+        for idx, track in enumerate(collection):
+            track.createAnalyticalFeature('TID', idx+1)
+            track.createAnalyticalFeature('MID', idx+1)
+
+        self.collection = collection
+
     
     def testPipeline(self):
-        print ("OK")
-        fmt = tkl.TrackFormat({'ext': 'CSV',
-                               'srid': 'ENU',
-                               'id_E': 1, 'id_N': 0, 'id_U': 3, 'id_T': 2,
-                               'time_fmt': '2D/2M/4Y 2h:2m:2s',
-                               'separator': ';',
-                               'header': 0,
-                               'cmt': '#',
-                               'read_all': True})
-        self.assertTrue("1=1", "premier test")
+
+        self.assertEqual(len(self.network.EDGES), 7, 'Number of edges=')
+        self.assertEqual(len(self.network.NODES), 8 ,'Number of nodes')
+
+
+
 
 
 
